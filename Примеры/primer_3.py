e1459b72eb83747c1960bf4d51665f6660557b55
Fedorova_Darya <fdu12345@mail.ru>
2026-01-28 04:05:16 +0300

    Правки

--- Примеры/primer_3.py
+++ Примеры/primer_3.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-n = 0
-for i in range(1, n + 1):
-
-
 def recursion(n):
     if n == 1:
         return 1
 
-    return n + recursion(n - 1)
+    return n + recursion(n-1)
+
+print(recursion(5))
+
+
+
+
+
